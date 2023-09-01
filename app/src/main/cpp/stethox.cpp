8c37755750ab8aa7d3274274d9e321e012602492
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2023-09-01 15:07:22 +0800

    Remove redundant comments

--- app/src/main/cpp/stethox.cpp
+++ app/src/main/cpp/stethox.cpp
@@ -1,22 +1,5 @@
 #include <jni.h>
 
-// Write C++ code here.
-//
-// Do not forget to dynamically load the C++ library into your application.
-//
-// For instance,
-//
-// In MainActivity.java:
-//    static {
-//       System.loadLibrary("stethox");
-//    }
-//
-// Or, in MainActivity.kt:
-//    companion object {
-//      init {
-//         System.loadLibrary("stethox")
-//      }
-//    }
 #include "classloader.h"
 #include "art.h"
 
